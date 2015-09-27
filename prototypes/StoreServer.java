6e82ff6e0c5073b6cacd4f3c0963bd251ecfaf46
mapleknight <karlwang1118@gmail.com>
2015-09-27 14:28:45 -0400

    update StoreServer

--- prototypes/StoreServer.java
+++ prototypes/StoreServer.java
@@ -1,75 +1,90 @@
-import java.math.BigDecimal;
 import java.net.Socket;
 import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.Map;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
-import java.net.UnknownHostException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-public class store_server {
-	public static void main(String args[]) throws IOException{
-		if(args.length != 1){
-			System.out.println("Usage : java Store_server port");
+
+public class StoreServer {
+
+	public static Map<String, String> storage = new HashMap<String, String>();
+
+	public static void main(String args[]) throws Exception {
+		if (args.length != 1) {
+			System.out.println("Usage : java StoreServer port");
 		}
 		int port = Integer.parseInt(args[0]);
 		ServerSocket serversocket = new ServerSocket(port);
 		System.out.println("Start Server on port :" + port);
-		try{
-			while(true){
+		try {
+			while (true) {
 				Socket clientSocket = serversocket.accept();
 				System.out.println("\nAccept connection from client");
 				process(clientSocket);
 			}
-		}catch(IOException e){
+		} catch (IOException e) {
 			System.out.println("Error Connection");
 		}
-		
-		//System.exit(0);
 	}
-	
-	public static void process(Socket clientSocket)throws IOException{
-		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
-		out.println("Welcome to the store system");
-		String UserInput;
-		String value = "";
-		String getfirst = "Hava not set any value.";
-		String setting = "The value has been set";
-		String result = "";
-		int flag = 1;
-		if((UserInput = in.readLine()) == null){
+
+	public static void process(Socket clientSocket) throws IOException {
+		
+		// open up IO streams
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				clientSocket.getInputStream()));
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+		/* Write a welcome message to the client */
+		out.println("Welcome to store server!");
+
+		/* read and print the client's request */
+		// readLine() blocks until the server receives a new line from client
+		String userInput;
+		if ((userInput = in.readLine()) == null) {
 			System.out.println("Error reading message");
-			in.close();
 			out.close();
+			in.close();
 			clientSocket.close();
+			return;
 		}
-		System.out.println("message " + UserInput);
-		String[] params = UserInput.split(" ");
-		if(params.length > 2) {
-			out.println("you must input as 'set <value>' or 'get'");
-			flag = 0;
+
+		System.out.println("Received message: " + userInput);
+
+		String[] params = userInput.split(" ");
+		if (params.length < 2) {
+			out.println("Input error, please input enough parameters!");
+
+			// close IO streams, then socket
+			out.close();
+			in.close();
+			clientSocket.close();
+			return;
 		}
-		else if(params[0].equalsIgnoreCase("set")){
-			result = setting;
-			value = params[1];
-			flag = 2;
-		}
-		else if(params[0].equalsIgnoreCase("get") && params.length == 1){
-			result = value;
-			flag = 2;
-		}
-		if(flag !=0 ){
-			try{
-				out.println(result);
-			}catch(Exception e){
-				out.println("we can only set a value or get that value.");
-				out.close();
-				in.close();
-				clientSocket.close();
-				continue;
+
+		if (params[0].equalsIgnoreCase("set")) {
+			if(params.length < 3) {
+				out.println("Input error, please input enough parameters!");
+			} else {
+				// set value
+				storage.put(params[1], params[2]);
+				out.println("Store successfully!");
 			}
+		} else if (params[0].equalsIgnoreCase("get")) {
+			// get value
+			if(storage.containsKey(params[1])) {
+				out.println(storage.get(params[1]));
+			} else {
+				out.println("There is no matched value stored!");
+			}
+		} else {
+			out.println("Input error, we can only handle get or set request!");
 		}
+
+		// close IO streams, then socket
+		out.close();
+		in.close();
+		clientSocket.close();
 	}
 }
-
